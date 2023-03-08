6d9a3cf2ef623e59a75f0482ea68630ada1aa729
goldddac1 <123153869+goldddac1@users.noreply.github.com>
2023-03-08 17:18:21 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,47 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
+
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.set('html', 'forms')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(express.static('html'))
+app.use(express.static('php'))
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+const mongoose = require('mongoose')
+const { compile } = require('ejs')
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
+app.post('/contact', urlencodedParser, function(req, res){
+  if (!req.body) return res.sendStatus(400);
+console.log(req.body);
+res.render('contact.html');
+
+});
+
+app.listen(process.env.PORT || 3000)
